2c4d4851c055bbd19f388ece9e92a4b68847d57c
Emma Smith <tarinaky@gmail.com>
2021-05-16 03:03:34 +0100

    Add stub for release command

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def queue(args):
         for gcode in args.g:
             queue_file.add_gcode(gcode)
 
+def release(args):
+    print(args)
+    with printhatqueue.QueueFile('queue.zip') as queue_file:
+        for printer in args.p:
+            queue_file.release_to_printer(args.p)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Manage a queue of gcode files that can be dispatched to octoprint')
     subparsers = parser.add_subparsers()
@@ -16,7 +22,8 @@ if __name__ == '__main__':
     scan_parser.set_defaults(func=queue)
 
     release_parser = subparsers.add_parser('release', help='release the next job to a printer')
-    release_parser.add_argument('-p', required=True, type=str, help='the address of the printer')
+    release_parser.add_argument('-p', required=True, action='append', type=str, help='the address of the printer')
+    release_parser.set_defaults(func=release)
 
 
     args = parser.parse_args()
